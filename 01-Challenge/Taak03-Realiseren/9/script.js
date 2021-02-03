8301cf5f187f33ad86c11357197fedb521972983
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-02-03 14:40:51 +0100

    save changes

---
+++ 01-Challenge/Taak03-Realiseren/9/script.js
@@ -0,0 +1,21 @@
+var bezoeker={
+    naam : prompt("Type aub uw naam in"),
+   leeftijd : prompt("Wat is je leeftijd?"),
+};
+if (bezoeker.naam != null) {
+   let koptekst = document.querySelector("h2");
+koptekst.innerHTML = "Welcome ! " + bezoeker.naam + " to my ";
+
+console.log(bezoeker);
+
+
+if (bezoeker.leeftijd >=18) {
+  koptekst.style.color= 'lightgreen';
+   }
+    if (bezoeker.leeftijd <18) {
+       koptekst.style.color = 'red';
+       }
+        if (bezoeker.leeftijd ==0) {
+           koptekst.style.color = 'yellow';
+                   }
+               }
